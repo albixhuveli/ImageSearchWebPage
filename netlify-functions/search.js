d2ff5d524ed94f11a632f21d8216089cb3673a4d
albixhuveli <albi.xhuveli.601@gmail.com>
2024-09-16 18:15:25 -0400

    added server operation

--- netlify-functions/search.js
+++ netlify-functions/search.js
@@ -1,21 +1,21 @@
-// netlify-functions/search.js
-const fetch = require('node-fetch');
+import fetch from 'node-fetch';
 
-exports.handler = async function(event, context) {
-    const query = event.queryStringParameters.query;
-    const token = process.env.UNSPLASH_API_KEY; // Use environment variable
+export async function handler(event) {
+  const query = event.queryStringParameters.query;
+  const token = process.env.UNSPLASH_API_KEY; // Use environment variable for API key
 
-    try {
-        const response = await fetch(`https://api.unsplash.com/search/photos?query=${query}&client_id=${token}`);
-        const data = await response.json();
-        return {
-            statusCode: 200,
-            body: JSON.stringify(data),
-        };
-    } catch (error) {
-        return {
-            statusCode: 500,
-            body: JSON.stringify({ error: 'Failed to fetch images' }),
-        };
-    }
-};
+  try {
+    const response = await fetch(`https://api.unsplash.com/search/photos?query=${query}&client_id=${token}`);
+    const data = await response.json();
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Failed to fetch images' }),
+    };
+  }
+}
+
